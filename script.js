7e18eb9dc9bcf60991b62681f7e8e46b5748d64b
Gustavo <gustavocandido044@gmail.com>
2025-07-16 10:31:01 -0300

    Adiciona áudios compatíveis com IOS

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
-// Função para tocar a faixa
 function playTrack(audioFile, title) {
   const audioPlayer = document.getElementById("audioPlayer");
+  const audioSource = document.getElementById("audioSource");
   const audioTitle = document.getElementById("audioTitle");
 
-  // Atualizar o título da música
   audioTitle.innerText = title;
-
-  // Alterar o arquivo de áudio
-  audioPlayer.src = audioFile;
-
-  // Reproduzir o áudio
-  audioPlayer.play();
-}
-
-// Função para pausar o áudio (se quiser usar no futuro)
-function pauseTrack() {
-  const audioPlayer = document.getElementById("audioPlayer");
-  audioPlayer.pause();
+  audioSource.src = audioFile;
+  audioPlayer.load(); // Recarrega o áudio
+  audioPlayer.play(); // Só vai funcionar se houver interação prévia no iOS
 }
